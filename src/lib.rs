516c0550817f54f6bc61793783d0d8bf0c41773a
m4h7 <m4h7@users.noreply.github.com>
2016-05-29 14:42:32 +0100

    add more lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,8 @@
+#![deny(missing_copy_implementations,
+        trivial_casts,
+        trivial_numeric_casts,
+        unstable_features,
+        unused_import_braces,
+        unused_qualifications)]
 pub mod reader;
 pub mod writer;
