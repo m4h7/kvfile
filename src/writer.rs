ae688f488bd6e8c764a21ec34f31b4e2956c1978
m4h7 <m4h7@users.noreply.github.com>
2016-05-29 14:40:59 +0100

    apply rustfmt

--- src/writer.rs
+++ src/writer.rs
@@ -6,29 +6,28 @@ use std::error::Error;
 
 pub struct KVFileWriter {
     f: File,
-    pos : usize
+    pos: usize,
 }
 
 // u32 to little endian u8 array
 #[inline]
 fn u32tou8ale(v: u32) -> [u8; 4] {
-    [
-        v as u8,
-        (v >> 8) as u8,
-        (v >> 16) as u8,
-        (v >> 24) as u8,
-    ]
+    [v as u8, (v >> 8) as u8, (v >> 16) as u8, (v >> 24) as u8]
 }
 
 
 impl KVFileWriter {
-    pub fn new(s : &str) -> KVFileWriter {
+    pub fn new(s: &str) -> KVFileWriter {
         let path = Path::new(s);
         let file = match File::create(&path) {
-            Err(why) => panic!("could not open file: {}: {}", path.display(), Error::description(&why)),
+            Err(why) => {
+                panic!("could not open file: {}: {}",
+                       path.display(),
+                       Error::description(&why))
+            }
             Ok(file) => file,
         };
-        KVFileWriter { f : file, pos : 0 }
+        KVFileWriter { f: file, pos: 0 }
     }
 
     fn write_header(self: &mut KVFileWriter, key: &[u8], value: &[u8]) {
@@ -44,42 +43,42 @@ impl KVFileWriter {
                         assert!(bwritten_v == value.len());
                         self.pos += bwritten_v;
                     }
-                    Err(e) => panic!("error writing value {:}", Error::description(&e))
+                    Err(e) => panic!("error writing value {:}", Error::description(&e)),
                 }
             }
-            Err(e) => panic!("error writing key {:}", e)
+            Err(e) => panic!("error writing key {:}", e),
         }
     }
 
     #[inline]
-    fn align_position(self: &mut KVFileWriter, align : usize) {
+    fn align_position(self: &mut KVFileWriter, align: usize) {
         if self.pos % align != 0 {
             let diff = align - (self.pos % align);
-            let empty : [u8; 16] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-            match self.f.write(&empty[0 .. diff]) {
+            let empty: [u8; 16] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+            match self.f.write(&empty[0..diff]) {
                 Ok(bwritten) => self.pos += bwritten,
-                Err(e) => panic!("error writing alignment bytes: {:}", Error::description(&e))
+                Err(e) => panic!("error writing alignment bytes: {:}", Error::description(&e)),
             }
         }
         assert!(self.f.seek(SeekFrom::Current(0)).ok().unwrap() % 4 == 0);
     }
 
-    pub fn put(self: &mut KVFileWriter, key: &[u8], value: &[u8])  {
+    pub fn put(self: &mut KVFileWriter, key: &[u8], value: &[u8]) {
         self.align_position(4);
         self.write_header(key, value);
         match self.f.write(key) {
             Ok(b_written_k) => {
                 assert!(b_written_k == key.len());
                 self.pos += b_written_k;
-            },
-            Err(e) => panic!("error writing key {:}", Error::description(&e))
+            }
+            Err(e) => panic!("error writing key {:}", Error::description(&e)),
         }
         match self.f.write(value) {
             Ok(b_written_v) => {
                 assert!(b_written_v == value.len());
                 self.pos += b_written_v;
-            },
-            Err(e) => panic!("error writing value {:}", Error::description(&e))
+            }
+            Err(e) => panic!("error writing value {:}", Error::description(&e)),
         }
     }
 }
