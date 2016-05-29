5be59376238cf7751ea8f42fc2640ddb3481da5b
m4h7 <m4h7@users.noreply.github.com>
2016-05-29 14:28:35 +0100

    implemented basic reader/writer functionality

---
+++ src/reader.rs
@@ -0,0 +1,59 @@
+extern crate memmap;
+
+use std::path::Path;
+use std::error::Error;
+use self::memmap::{Mmap, Protection};
+
+#[inline]
+fn u8aletou32(ptr : &[u8]) -> u32 {
+    (ptr[0] as u32) | ((ptr[1] as u32) << 8) | ((ptr[2] as u32) << 16) | ((ptr[3] as u32) << 24)
+}
+
+pub struct KVFileReader {
+    m: Mmap
+}
+
+impl KVFileReader {
+
+    pub fn new(s : &str) -> KVFileReader {
+        let path = Path::new(s);
+
+        let mmap = match Mmap::open_path(path, Protection::Read) {
+            Err(why) => panic!("could not open file: {}: {}", path.display(), Error::description(&why)),
+            Ok(file) => file,
+        };
+
+        KVFileReader { m : mmap }
+    }
+
+    pub fn key(&self, pos : usize) -> &[u8] {
+        let ptr = unsafe { self.m.as_slice() };
+        let len = u8aletou32(&ptr[pos .. pos + 4]) as usize;
+        return &ptr[pos + 8 .. (pos + 8 + len)];
+    }
+
+    pub fn value(&self, pos : usize) -> &[u8] {
+        let ptr = unsafe { self.m.as_slice() };
+        let keylen = u8aletou32(&ptr[pos .. pos + 4]) as usize;
+        let valuelen = u8aletou32(&ptr[pos + 4 .. pos + 8]) as usize;
+        return &ptr[(pos + 8 + keylen) .. (pos + 8 + keylen + valuelen)];
+    }
+
+    pub fn next(&self, pos : usize) -> usize{
+        let ptr = unsafe { self.m.as_slice() };
+        let keylen = u8aletou32(&ptr[pos .. pos + 4]) as usize;
+        let valuelen = u8aletou32(&ptr[pos + 4 .. pos + 8]) as usize;
+        let mut newpos = pos + 4 + 4 + keylen + valuelen;
+
+        let align = 4;
+        if newpos % align != 0 {
+            let diff = align - (pos % align);
+            newpos += diff;
+        }
+        newpos
+    }
+
+    pub fn len(&self) -> usize {
+        self.m.len()
+    }
+}
---
+++ src/writer.rs
@@ -0,0 +1,85 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::SeekFrom;
+use std::path::Path;
+use std::error::Error;
+
+pub struct KVFileWriter {
+    f: File,
+    pos : usize
+}
+
+// u32 to little endian u8 array
+#[inline]
+fn u32tou8ale(v: u32) -> [u8; 4] {
+    [
+        v as u8,
+        (v >> 8) as u8,
+        (v >> 16) as u8,
+        (v >> 24) as u8,
+    ]
+}
+
+
+impl KVFileWriter {
+    pub fn new(s : &str) -> KVFileWriter {
+        let path = Path::new(s);
+        let file = match File::create(&path) {
+            Err(why) => panic!("could not open file: {}: {}", path.display(), Error::description(&why)),
+            Ok(file) => file,
+        };
+        KVFileWriter { f : file, pos : 0 }
+    }
+
+    fn write_header(self: &mut KVFileWriter, key: &[u8], value: &[u8]) {
+        let key_length = key.len() as u32;
+        let value_length = value.len() as u32;
+        match self.f.write(&u32tou8ale(key_length)) {
+            Ok(bwritten_k) => {
+                assert!(bwritten_k == key.len());
+                self.pos += bwritten_k;
+
+                match self.f.write(&u32tou8ale(value_length)) {
+                    Ok(bwritten_v) => {
+                        assert!(bwritten_v == value.len());
+                        self.pos += bwritten_v;
+                    }
+                    Err(e) => panic!("error writing value {:}", Error::description(&e))
+                }
+            }
+            Err(e) => panic!("error writing key {:}", e)
+        }
+    }
+
+    #[inline]
+    fn align_position(self: &mut KVFileWriter, align : usize) {
+        if self.pos % align != 0 {
+            let diff = align - (self.pos % align);
+            let empty : [u8; 16] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+            match self.f.write(&empty[0 .. diff]) {
+                Ok(bwritten) => self.pos += bwritten,
+                Err(e) => panic!("error writing alignment bytes: {:}", Error::description(&e))
+            }
+        }
+        assert!(self.f.seek(SeekFrom::Current(0)).ok().unwrap() % 4 == 0);
+    }
+
+    pub fn put(self: &mut KVFileWriter, key: &[u8], value: &[u8])  {
+        self.align_position(4);
+        self.write_header(key, value);
+        match self.f.write(key) {
+            Ok(b_written_k) => {
+                assert!(b_written_k == key.len());
+                self.pos += b_written_k;
+            },
+            Err(e) => panic!("error writing key {:}", Error::description(&e))
+        }
+        match self.f.write(value) {
+            Ok(b_written_v) => {
+                assert!(b_written_v == value.len());
+                self.pos += b_written_v;
+            },
+            Err(e) => panic!("error writing value {:}", Error::description(&e))
+        }
+    }
+}
